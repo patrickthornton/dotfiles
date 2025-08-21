8fafcd475285ffe00b9edd30df3d172d90eacc72
patrick thornton <patrickthornton@g.harvard.edu>
2025-08-21 16:45:54 -0400

    three weeks of bad macros for work

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ if status is-login
 end
 
 # file listing
-abbr -a l eza -1 -l -a --total-size --no-user --no-permissions --icons=always
+abbr -a l eza -1 -l -a --no-user --no-permissions --icons=always
 abbr -a lg eza -G -l -a --total-size --no-user --no-permissions --icons=always
 abbr -a lt eza -T -l -a --total-size --no-user --no-permissions --icons=always
 
@@ -23,16 +23,17 @@ abbr -a bt btop
 
 # chdir
 abbr -a c cd
-abbr -a cb cd ..
+abbr -a cb cd .. # cd back
+abbr -a cr cd - # cd return
 
 # config editing
-abbr -a cf zed ~/dotfiles/fish
-abbr -a cn zed ~/dotfiles/nushell
-abbr -a ca zed ~/dotfiles/alacritty
-abbr -a cs zed ~/dotfiles/starship
-abbr -a ch zed ~/dotfiles/helix
-abbr -a cz zed ~/dotfiles/zellij
-abbr -a cg zed ~/dotfiles/gdb/.gdbinit
+abbr -a cf zed-preview ~/dotfiles/fish
+abbr -a cn zed-preview ~/dotfiles/nushell
+abbr -a ca zed-preview ~/dotfiles/alacritty
+abbr -a cs zed-preview ~/dotfiles/starship
+abbr -a ch zed-preview ~/dotfiles/helix
+abbr -a cz zed-preview ~/dotfiles/zellij
+abbr -a cg zed-preview ~/dotfiles/gdb/.gdbinit
 
 # localizing config files
 abbr -a cpg cp ~/dotfiles/.gitignore .gitignore
@@ -90,7 +91,8 @@ abbr -a otr cargo test --release
 abbr -a otrn cargo test --release -- --nocapture
 abbr -a oy cargo clippy
 
-abbr -a op open .
+abbr -a op open
+abbr -a oh open .
 
 # Directory creation and navigation function
 function md
@@ -112,7 +114,8 @@ abbr -a q exit
 
 abbr -a rd rm -rf
 abbr -a s starship
-abbr -a zh 'zed .'
+abbr -a zp zed-preview
+abbr -a zh zed-preview .
 
 # line counter
 abbr -a tk tokei
@@ -126,9 +129,91 @@ abbr -a ua uv add
 abbr -a ur uv remove
 abbr -a us uv sync
 
+abbr -a y yarn
+abbr -a yt yarn test
+abbr -a ytc yarn test --coverage
+
 abbr -a b2 /usr/local/bin/brew
 
 abbr -a abcdefghijklmnopqrstuvwxyz echo yay!!
 
+fish_add_path /opt/homebrew/bin
+fish_add_path /usr/local/bin
+set fish_greeting
+
+abbr -a r source ~/.config/fish/config.fish
+
 zoxide init fish | source
 starship init fish | source
+
+# fern
+function fgen
+    argparse 'n/no-prev' 'l/local' -- $argv
+    or return
+
+    set -l cmd fern generate --group $argv[1]-sdk --preview --log-level debug
+    if set -ql _flag_n
+        set cmd (string replace --all -- '--preview' '' $cmd)
+    end
+    if set -ql _flag_l
+        set cmd (string replace --all -- 'fern' 'FERN_NO_VERSION_REDIRECTION=true node ~/fern/fern/packages/cli/cli/dist/prod/cli.cjs' $cmd)
+    end
+    eval $cmd
+end
+
+function fdef
+    argparse 'l/local' -- $argv
+    or return
+
+    set -l cmd fern write-definition
+    if set -ql _flag_l
+        set cmd (string replace --all -- 'fern' 'FERN_NO_VERSION_REDIRECTION=true node ~/fern/fern/packages/cli/cli/dist/prod/cli.cjs' $cmd)
+    end
+    eval $cmd
+end
+
+function javaver
+    set -x -g JAVA_HOME (/usr/libexec/java_home -v $argv[1])
+end
+abbr -a gw './gradlew'
+
+function seedgen
+    argparse 'o/output-to-seed' 's/scripts' -- $argv
+    or return
+
+    set -l cmd pnpm seed run --generator $argv[1]-sdk --path ~/sdks/$argv[2]-fern-config/fern --log-level debug --skipScripts
+    if set -ql _flag_o
+        set cmd $cmd --output-path ~/.seed/$argv[2]-$argv[1]
+    end
+    if set -ql _flag_s
+        set cmd (string replace --all -- '--skipScripts' '' $cmd)
+    end
+    eval $cmd
+end
+
+function seedtest
+    argparse 's/scripts' -- $argv
+    or return
+
+    set -l cmd pnpm seed test --generator $argv[1]-sdk --fixture $argv[2] --outputFolder $argv[3] --log-level debug --skipScripts
+    if set -ql _flag_s
+        set cmd (string replace --all -- '--skipScripts' '' $cmd)
+    end
+    eval $cmd
+end
+
+abbr -a fbuild 'pnpm install && pnpm fern:build'
+abbr -a sbuild 'pnpm install && pnpm seed:build'
+abbr -a build 'pnpm install && pnpm fern:build && pnpm seed:build'
+abbr -a lfern FERN_NO_VERSION_REDIRECTION=true node ~/fern/fern/packages/cli/cli/dist/prod/cli.cjs
+
+abbr -a ghs gh auth switch
+
+set -x -g PNPM_HOME /Users/Patrick/Library/pnpm
+
+fish_add_path /Users/Patrick/Library/pnpm
+
+abbr -a obliterate git clean -fdx
+
+abbr -a frond pnpm frond
+abbr -a shotsnap pnpm test:update --continue=always
